17db5aeb5ab0d287eb77f205c8e384d71ef8d186
Byran <sunhaolin@steedos.com>
2025-07-04 08:41:48 +0000

    @steedos-labs/template-contract@3.0.1

--- steedos-packages/contract/src/index.js
+++ steedos-packages/contract/src/index.js
@@ -30,7 +30,7 @@ module.exports = {
             name: project.name,
             path: path.join(__dirname, ".."),
             isPackage: true,
-            // isUnmanaged: true
+            isUnmanaged: true
         }
     },
 
